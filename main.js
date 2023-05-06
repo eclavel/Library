df8c80f24f6429eb81f3191a53446a905591b1b8
eclavel <113940257+eclavel@users.noreply.github.com>
2023-05-05 17:27:09 -0700

    change book status when clicked

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const addBook = document.getElementById('addBook');
 const popup = document.getElementById('form-container');
+const BookStatus = document.querySelectorAll('.book-status');
 const myLibrary = [];
 
 function Book(author, title, pagesRead, completed) {
@@ -13,6 +14,20 @@ function addBookToLibrary() {
   // do stuff here
 }
 
+// Popup for the form when clicked
 addBook.addEventListener('click', () =>{
   popup.style.top = '50px';
+});
+// Change Book Status 
+BookStatus.forEach(book => {
+  book.addEventListener('click', () =>{
+    if(book.innerHTML === 'READ' ){
+      book.style.background = 'orange';
+      book.innerHTML = 'UNREAD';
+    }
+    else{
+      book.style.background = 'greenyellow';
+      book.innerHTML = 'READ';
+    } 
+})
 });
